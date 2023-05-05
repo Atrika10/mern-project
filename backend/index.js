6ac81d9dfc942f39519a402c38017f221c580050
ATRIKA SHOW <readeratrika08@gmail.com>
2023-05-05 21:03:21 +0530

    adding validation using express-validator

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,10 @@ const express = require('express');
 connectToMongo();
 
 const app = express()
-const port = 3000
+const port = 5000
+
+// to use req body
+app.use(express.json())
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,32 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
+const User =  require('../models/User');
 const router = express.Router();
 
-router.get('/', (req, res)=>{
-    obj = {
-        name : 'Atrika',
-        age : 21
+
+router.post('/', [
+
+    body('name', 'enter a valid name').isLength({ min: 3 }),
+    body('email', 'enter a valid mail').isEmail(),
+    // password must be at least 5 chars long
+    body('password', 'password must be at 5 characters').isLength({ min: 5 }),
+   
+],  (req, res)=>{
+    // console.log(req.body);
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    res.json(obj);
+    
+    User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+      }).then(user => res.json(user))
+      .catch(err => {console.log(err)
+        res.json({error : 'please enter a unique value for email', message : err.message})});
+
+    // res.send(req.body);
 });
 module.exports = router;
