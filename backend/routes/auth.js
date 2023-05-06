7b7411ec67ea252b9ec436da755c1e8112cf77e6
ATRIKA SHOW <readeratrika08@gmail.com>
2023-05-06 19:23:24 +0530

    generating hashing password and jsonwebtoken

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,32 +1,66 @@
 const express = require('express');
 const { body, validationResult } = require('express-validator');
+
+const bcrypt = require('bcryptjs'); // for generating hash with salt 
+const jwt = require('jsonwebtoken'); // for generating web token
+
 const User =  require('../models/User');
 const router = express.Router();
 
+const JWT_SECRET = 'atrika@show';
 
-router.post('/', [
+// creating a user using : POST "api/auth/createuser". no login required
+router.post('/createuser', [
 
+    // adding validation
     body('name', 'enter a valid name').isLength({ min: 3 }),
     body('email', 'enter a valid mail').isEmail(),
-    // password must be at least 5 chars long
     body('password', 'password must be at 5 characters').isLength({ min: 5 }),
    
-],  (req, res)=>{
+], async (req, res)=>{
     // console.log(req.body);
 
+    // If there are errors, return Bad request & the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
     
-    User.create({
-        name: req.body.name,
-        password: req.body.password,
-        email: req.body.email,
-      }).then(user => res.json(user))
-      .catch(err => {console.log(err)
-        res.json({error : 'please enter a unique value for email', message : err.message})});
+    try {
+            // Check whether the user with this email exist already
+        let user = await User.findOne({email: req.body.email});
+        if(user){
+            return res.status(400).json({error : "a user with this email already exist"});
+        }
 
-    // res.send(req.body);
+        // creating a salt to add with user's password
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt) ;
+
+        // if user don't exist then create a user 
+         user = await User.create({
+            name: req.body.name,
+            password: secPass,
+            email: req.body.email,
+        });
+
+        // generating jwtToken for new user
+        const data = {
+            user : {
+                id : user.id
+            }
+        }
+        const authToken = jwt.sign(data,JWT_SECRET);
+        console.log(authToken);
+    
+        res.json({authToken});
+        // res.json({user});
+        // res.send(req.body);
+        
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("some error occured");
+    }
+   
 });
 module.exports = router;
