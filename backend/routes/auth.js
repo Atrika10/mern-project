0d9641e902077d6e79d556138bbecd75eb89062f
ATRIKA SHOW <readeratrika08@gmail.com>
2023-05-07 08:56:34 +0530

    Create Login route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 const JWT_SECRET = 'atrika@show';
 
-// creating a user using : POST "api/auth/createuser". no login required
+// ROUTE : 1  creating a user using : POST "api/auth/createuser". no login required
 router.post('/createuser', [
 
     // adding validation
@@ -59,8 +59,51 @@ router.post('/createuser', [
         
     } catch (error) {
         console.log(error.message);
-        res.status(500).send("some error occured");
+        res.status(500).send("internal server Error");
     }
    
 });
+
+
+// ROUTE : 2 authenticate a user using : POST "api/auth/login". 
+router.post('/login', [
+    body('email', 'enter a valid mail').isEmail(),
+    body('password', 'password cannot be blank').exists(),
+   
+], async (req, res)=>{
+    // If there are errors, return Bad request & the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // after getting valid mail & password
+    const {email, password} = req.body;
+    try {
+        let user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({error : "please try to login with correct credentials."});
+        }
+
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if(!passwordCompare){
+            return res.status(400).json({error : "please try to login with correct credentials."});
+        }
+        // if it is correct
+        const data = {
+            user : {
+                id : user.id
+            }
+        }
+        const authToken = jwt.sign(data,JWT_SECRET);
+        console.log(authToken);
+    
+        res.json({authToken});
+
+        
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("internal server Error");
+    }
+})
 module.exports = router;
