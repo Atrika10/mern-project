ef1bf31d5148c75564ec8e7f6da2c5b1fc24e356
ATRIKA SHOW <readeratrika08@gmail.com>
2023-05-12 22:57:25 +0530

    signup working

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -25,7 +25,7 @@ router.post('/createuser', [
     // If there are errors, return Bad request & the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({success, errors: errors.array() });
     }
     
     try {
